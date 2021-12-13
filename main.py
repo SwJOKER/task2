dd6fd1490f49526b19982b0a2e79da4901a59c76
SwJOKER <63502467+SwJOKER@users.noreply.github.com>
2021-12-13 16:54:47 +0300

    Приблизительно, то что надо

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 # This is a sample Python script.
 import logging
+import time
 
 import dirsync as ds
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from dirsync import sync
 import logging
+import sys
+DIR_ONE = sys.argv[1]
+DIR_TWO = sys.argv[2]
+PERIOD = sys.argv[3]
 
-def print_hi(name):
+def syncronize_dir():
     file_log = logging.FileHandler('Log.log')
     console_out = logging.StreamHandler()
-
     logging.basicConfig(handlers=(file_log, console_out),
                         format='[%(asctime)s | %(levelname)s]: %(message)s',
                         datefmt='%m.%d.%Y %H:%M:%S',
                         level=logging.INFO)
-    #logging.basicConfig(filename="sync.log", level=logging.DEBUG)
     my_log = logging.getLogger('dirsync')
-    sync('./test1', './test2', 'sync', purge=True, verbose=True, logger=my_log)
+    while True:
+        sync(DIR_ONE, DIR_TWO, 'sync', purge=True, verbose=True, logger=my_log)
+        time.sleep(int(PERIOD))
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    syncronize_dir()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
