39cac9388579a7d59a013310fe19c1212f269581
SwJOKER <63502467+SwJOKER@users.noreply.github.com>
2021-12-14 13:51:53 +0300

    +delete_garbage удаляет лишнюю информацию из лога

--- main.py
+++ main.py
@@ -2,33 +2,50 @@
 import logging
 import time
 
-import dirsync as ds
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from dirsync import sync
 import logging
 import sys
+
 DIR_ONE = sys.argv[1]
 DIR_TWO = sys.argv[2]
 PATH_LOG = sys.argv[3]
 PERIOD = sys.argv[4]
+#не регулярные выражения, главное работает: в файле лога оставляет только нужные операции.
+def delete_garbage():
+    f = open(PATH_LOG, 'r')
+    lines = f.readlines()
+    f.close()
+    f = open(PATH_LOG, 'w')
+    for line in lines:
+        msg = line.split('|')[1]
+        if msg.find("Updating") != -1:
+            f.write(line)
+        if msg.find("Copying") != -1:
+            f.write(line)
+        if msg.find("Deleting") != -1:
+            f.write(line)
+        if msg.find("created") != -1:
+            f.write(line)
+    f.close()
+
+
 
 def syncronize_dir():
     file_log = logging.FileHandler(PATH_LOG)
     console_out = logging.StreamHandler()
     logging.basicConfig(handlers=(file_log, console_out),
-                        format='[%(asctime)s | %(levelname)s]: %(message)s',
+                        format='[%(asctime)s | %(message)s',
                         datefmt='%m.%d.%Y %H:%M:%S',
                         level=logging.INFO)
     my_log = logging.getLogger('dir_syncronize')
     while True:
         sync(DIR_ONE, DIR_TWO, 'sync', purge=True, verbose=True, logger=my_log)
+        delete_garbage()
         time.sleep(int(PERIOD))
-
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     syncronize_dir()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
